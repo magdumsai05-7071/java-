7863bd2a453519ad12aa3d29ee3ec2d771bf6a23
sumit prakash magdum <magdumsai05@gmail.com>
2026-02-04 12:22:07 +0530

    exception handling & multithreading
    multithreading 
    life cycle of thread 
    thread exceptions
    inter thread communication 
    deadlock
    error and exception 
    built in exception 
    throwing own exceptions

---
+++ 14.topic/thread.java
@@ -0,0 +1,16 @@
+
+
+// class mythread extends thread{
+//     public void run(){
+//         System.out.print("thread run");
+//     }
+
+
+// public static void main(String[]args){
+//     mythread t = new mythread();
+//     t.start();
+// }
+
+// }
+
+
